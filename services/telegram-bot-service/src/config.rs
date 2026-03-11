34900011868b36f171875085a0704d21e0692698
DeathCrushZzZ <Rustforthefuture@yandex.ru>
2026-03-11 03:38:18 +0300

    feat(config): Added configuration loading from environment variables

--- services/telegram-bot-service/src/config.rs
+++ services/telegram-bot-service/src/config.rs
@@ -1,3 +1,4 @@
+use std::env;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn init_tracing() {
@@ -14,6 +15,40 @@ pub fn init_tracing() {
         .init();
 }
 
-pub fn run() {
-    println!("Hello, world!");
+#[derive(Debug, Clone)]
+pub struct AppConfig {
+    /// Токен Telegram-бота.
+    ///
+    /// Критически важный параметр:
+    /// без него приложение не имеет смысла продолжать работу.
+    pub bot_token: String,
+    /// URL подключения к базе данных.
+    ///
+    /// Используется при инициализации пула соединений.
+    /// Неверное значение приведёт к падению приложения на старте,
+    /// что является корректным поведением для backend-сервиса.
+    pub database_url: String,
+}
+
+impl AppConfig {
+    pub fn load_from_environment() -> Self {
+        // Читаем переменные окружения и создаём конфигурацию.
+        dotenvy::dotenv().ok();
+        tracing::debug!("Loading configuration from environment variables...");
+
+        // Загрузка токена Telegram-бота.
+        let bot_token =
+            env::var("BOT_TOKEN").expect("BOT_TOKEN is not set in environment variables");
+        tracing::debug!("Loaded BOT_TOKEN from environment variables.");
+
+        // Загрузка токена базы данных
+        let database_url =
+            env::var("DATABASE_URL").expect("DATABASE_URL is not set in environment variables");
+        tracing::debug!("Loaded DATABASE_URL from environment variables.");
+
+        Self {
+            bot_token,
+            database_url,
+        }
+    }
 }
--- services/telegram-bot-service/src/main.rs
+++ services/telegram-bot-service/src/main.rs
@@ -1,6 +1,9 @@
-use telegram_bot_service::config::init_tracing;
+use telegram_bot_service::config::{AppConfig, init_tracing};
 
 #[tokio::main]
 async fn main() {
     init_tracing();
+    tracing::info!("Starting Telegram Bot Service...");
+    let config = AppConfig::load_from_environment();
+    tracing::debug!("Config loaded successfully");
 }
